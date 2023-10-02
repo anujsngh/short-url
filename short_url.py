9eb6e234c1c02a9267e7d8f48b291c98f8ec223c
Anuj Singh <62144435+anujsngh@users.noreply.github.com>
2023-10-02 19:37:43 +0530

    Modify functional into OOP implementation and add get_long_url functionality. Remove bug to make the url conversion unique.

--- short_url.py
+++ short_url.py
@@ -3,70 +3,26 @@ import random
 import utils
 
 
-def generate_short_url(long_url:str, squish_url=False):
-    """
-    Function to create short url from long url.
-    """
-    # Generate a random integer value between 6 to 10 
-    # to decide length  of random string.
-    str_len = random.randint(6, 10)
+class ShortURLService():
+    def __init__(self) -> None:
+        pass
 
-    # Generate list of all the alphabets in lowercase.
-    lower_chars = string.ascii_lowercase
-
-    # Select characters randomely and join chars till 
-    # defined length is achieved.
-    short_str = "".join([random.choice(lower_chars) for _ in range(str_len)])
-
-    # Extract domain url from the long url.
-    domain_url  = utils.extract_domain_url(long_url)
-
-    # Check if squish is true or not.
-    if squish_url:
-        # Return the mask for long url.
-        short_url =  "https://www.shorturl.com" + "/" + short_str
-    else:
-        # Return the domain url with random string attached in the end as 
-        # mask for long url.
-        short_url =  domain_url + "/" + short_str
-
-    save_short_to_long_url(short_url, long_url)
-    save_long_to_short_url(short_url, long_url)
-
-    return short_url
+    def get_short_url(self, long_url:str, squish_url=False):
+        return utils.generate_short_url(long_url, squish_url)
     
+    def get_long_url(self, short_url:str):
+        return utils.generate_long_url(short_url)
 
-def save_short_to_long_url(short_url, long_url):
-    urls = utils.load_from_json(file_name="short_to_long.json")
-
-    if urls is None:
-        urls = {
-            short_url: long_url
-        }
-        utils.save_to_json(data=urls, file_name="short_to_long.json")
-    else:
-        if short_url not in urls:
-            urls[short_url] = long_url
-            utils.save_to_json(data=urls, file_name="short_to_long.json")
-
-
-def save_long_to_short_url(short_url, long_url):
-    urls = utils.load_from_json(file_name="long_to_short.json")
-
-    if urls is None:
-        urls = {
-            long_url: short_url
-        }
-        utils.save_to_json(data=urls, file_name="long_to_short.json")
-    else:
-        if long_url not in urls:
-            urls[long_url] = short_url
-            utils.save_to_json(data=urls, file_name="long_to_short.json")
 
 
 if __name__ == "__main__":
     # test 0
-    print(generate_short_url("https://www.google.com/update/new"))
+    a = ShortURLService()
+    print(a.get_short_url("https://www.google.com/update/new"))
 
     # test 1
-    print(generate_short_url("https://www.office.microsoft.com/services/powerpoint/create/new_ppt", squish_url=True))
+    print(a.get_short_url("https://www.office.microsoft.com/services/powerpoint/create/new_ppt", squish_url=True))
+
+    # test 2
+    print(a.get_long_url("https://www.shorturl.com/dyoqrzh"))
+
--- utils.py
+++ utils.py
@@ -1,5 +1,94 @@
 import json
 import re
+import random
+import string
+
+
+def generate_long_url(short_url:str):
+    urls = load_from_json(file_name="short_to_long.json")
+
+    if urls:
+        if short_url in urls:
+            return urls[short_url]
+    
+    return None
+
+
+def generate_short_url(long_url:str, squish_url=False):
+    """
+    Function to create short url from long url.
+    """
+
+    urls = load_from_json(file_name="long_to_short.json")
+
+    if urls:
+        if long_url in urls:
+            return urls[long_url]
+    
+    # Generate a random integer value between 6 to 10 
+    # to decide length  of random string.
+    str_len = random.randint(6, 10)
+
+    # Generate list of all the alphabets in lowercase.
+    lower_chars = string.ascii_lowercase
+
+    # Select characters randomely and join chars till 
+    # defined length is achieved.
+    short_str = "".join([random.choice(lower_chars) for _ in range(str_len)])
+
+    # Extract domain url from the long url.
+    domain_url  = extract_domain_url(long_url)
+
+    # Check if squish is true or not.
+    if squish_url:
+        # Return the mask for long url.
+        short_url =  "https://www.shorturl.com" + "/" + short_str
+    else:
+        # Return the domain url with random string attached in the end as 
+        # mask for long url.
+        short_url =  domain_url + "/" + short_str
+
+    urls = load_from_json(file_name="short_to_long.json")
+    if urls:
+        if short_url in urls:
+            return short_url
+    
+    # Saving  The short_url -> long_url and long_url -> short_url 
+    # for future references.
+    save_short_to_long_url(short_url, long_url)
+    save_long_to_short_url(short_url, long_url)
+
+    return short_url
+    
+
+
+def save_short_to_long_url(short_url, long_url):
+    urls = load_from_json(file_name="short_to_long.json")
+
+    if urls is None:
+        urls = {
+            short_url: long_url
+        }
+        save_to_json(data=urls, file_name="short_to_long.json")
+    else:
+        if short_url not in urls:
+            urls[short_url] = long_url
+            save_to_json(data=urls, file_name="short_to_long.json")
+
+
+def save_long_to_short_url(short_url, long_url):
+    urls = load_from_json(file_name="long_to_short.json")
+
+    if urls is None:
+        urls = {
+            long_url: short_url
+        }
+        save_to_json(data=urls, file_name="long_to_short.json")
+    else:
+        if long_url not in urls:
+            urls[long_url] = short_url
+            save_to_json(data=urls, file_name="long_to_short.json")
+
 
 
 def extract_domain_url(url):
